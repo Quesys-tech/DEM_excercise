64dad7528fddb2936d80dac8827e764778ab189f
Queue_sys <29653819+Quesys-tech@users.noreply.github.com>
2024-08-15 22:22:14 +0900

    rm: Aqua.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,10 +1,6 @@
 using DEM_excercise
 using Test
-using Aqua
 
 @testset "DEM_excercise.jl" begin
-    @testset "Code quality (Aqua.jl)" begin
-        Aqua.test_all(DEM_excercise)
-    end
     # Write your tests here.
 end
