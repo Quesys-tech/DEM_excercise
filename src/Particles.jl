d4c89e826b7b168854aa33def5dcdcd3ba275d89
Queue_sys <29653819+Quesys-tech@users.noreply.github.com>
2024-09-30 21:06:28 +0900

    add: constructor for ParticleCellList

--- src/Particles.jl
+++ src/Particles.jl
@@ -16,10 +16,26 @@ struct DEMParticles{T <: AbstractFloat} <: AbstractDEMParticle{T}
     _sorted::Bool
 end
 
-struct ParticleCellList{D, T <: AbstractFloat}
-    𝐥::SVector{D, T}
+struct ParticleCellList{D, T}
+    h::T
     𝐱_min::SVector{D, T}
     𝐱_max::SVector{D, T}
     min_id::Array{Int, D}
     max_id::Array{Int, D}
+    function ParticleCellList(h::T, 𝐱_min::SVector{D, T}, 𝐱_max::SVector{D, T}) where {D, T}
+        num_cell_dim = @. ceil(Int, (𝐱_max - 𝐱_min) / h)
+        min_id = zeros(Int, num_cell_dim...)
+        max_id = zeros(Int, num_cell_dim...)
+        new{D, T}(h, 𝐱_min, 𝐱_max, min_id, max_id)
+    end
+end
+
+@testitem "ParticleCellList" begin
+    using StaticArrays
+    cl = ParticleCellList(0.1, SVector(0.0, 0.0), SVector(1.0, 1.0))
+    @test cl.h == 0.1
+    @test cl.𝐱_min == SVector(0.0, 0.0)
+    @test cl.𝐱_max == SVector(1.0, 1.0)
+    @test cl.min_id == zeros(Int, 10, 10)
+    @test cl.max_id == zeros(Int, 10, 10)
 end
