975ce7b96a5461d5c6356ba1068bee084ecf62cb
Quesys-tech <29653819+Quesys-tech@users.noreply.github.com>
2024-09-30 12:42:07 +0000

    Format .jl files

--- docs/make.jl
+++ docs/make.jl
@@ -1,23 +1,23 @@
 using DEM_excercise
 using Documenter
 
-DocMeta.setdocmeta!(DEM_excercise, :DocTestSetup, :(using DEM_excercise); recursive=true)
+DocMeta.setdocmeta!(DEM_excercise, :DocTestSetup, :(using DEM_excercise); recursive = true)
 
 makedocs(;
-    modules=[DEM_excercise],
-    authors="Queue_sys <29653819+Quesys-tech@users.noreply.github.com> and contributors",
-    sitename="DEM_excercise.jl",
-    format=Documenter.HTML(;
-        canonical="https://Queue_sys.github.io/DEM_excercise.jl",
-        edit_link="main",
-        assets=String[],
+    modules = [DEM_excercise],
+    authors = "Queue_sys <29653819+Quesys-tech@users.noreply.github.com> and contributors",
+    sitename = "DEM_excercise.jl",
+    format = Documenter.HTML(;
+        canonical = "https://Queue_sys.github.io/DEM_excercise.jl",
+        edit_link = "main",
+        assets = String[]
     ),
-    pages=[
-        "Home" => "index.md",
-    ],
+    pages = [
+        "Home" => "index.md"
+    ]
 )
 
 deploydocs(;
-    repo="github.com/Queue_sys/DEM_excercise.jl",
-    devbranch="main",
+    repo = "github.com/Queue_sys/DEM_excercise.jl",
+    devbranch = "main"
 )
