54c556cd69987e9c91b3cfacefa4bc1b9d0d2af0
Queue_sys <29653819+Quesys-tech@users.noreply.github.com>
2024-09-30 21:06:12 +0900

    add: testing framework TestItems

--- src/DEM_excercise.jl
+++ src/DEM_excercise.jl
@@ -1,9 +1,11 @@
 module DEM_excercise
 using StaticArrays
 using LinearAlgebra
+using TestItems
 
 # Write your package code here.
 include("Particles.jl")
 include("System.jl")
 include("sorting.jl")
+export DEMParticles, ParticleCellList
 end
