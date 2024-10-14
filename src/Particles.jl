5ef0a12f041ee2f58445900660b7ba607990af94
Queue_sys <29653819+Quesys-tech@users.noreply.github.com>
2024-10-14 22:56:03 +0900

    add: constructor for DEMParticles

--- src/Particles.jl
+++ src/Particles.jl
@@ -19,16 +19,44 @@ A struct to store the information of the particles in the DEM simulation.
 struct DEMParticles{T <: AbstractFloat} <: AbstractDEMParticle{T}
     id::Vector{Int}
     type::Vector{Int}
-    𝛉::Matrix{T}
     m::Vector{T}
     Π::Vector{T}
     r::Vector{T}
     𝐱::Matrix{T}
+    𝛉::Matrix{T}
     𝐮::Matrix{T}
     𝛚::Matrix{T}
     _permutation::Vector{Int}
     _cell_id::Vector{Int}
     _sorted::Bool
+
+    function DEMParticles(type::Vector{Int}, m::Vector{T}, Π::Vector{T}, r::Vector{T},
+            𝐱::Matrix{T}, 𝛉::Matrix{T}, 𝐮::Matrix{T}, 𝛚::Matrix{T}) where {T}
+        N = length(m)
+        @assert N == length(Π) == length(r) == size(𝐱, 2) == size(𝛉, 2) == size(𝐮, 2) ==
+                size(𝛚, 2)
+        @assert size(𝐱, 1) == size(𝛉, 1) == size(𝐮, 1) == size(𝛚, 1) == 3
+
+        id = collect(1:N)
+        _permutation = collect(1:N)
+        _cell_id = zeros(Int, N)
+        _sorted = false
+        new{T}(id, type, m, Π, r, 𝐱, 𝛉, 𝐮, 𝛚, _permutation, _cell_id, _sorted)
+    end
+end
+@testitem "DEMParticles" begin
+    type = ones(Int, 10)
+    m = rand(10)
+    Π = rand(10)
+    r = rand(10) * 0.1
+    𝐱 = [7.4 6.9 7.4 0.8 1.0 5.4 9.5 0.1 4.4 3.5
+         3.8 9.9 2.1 1.4 0.4 6.6 2.7 1.4 6.2 0.3
+         0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0]
+    𝛉 = zero(𝐱)
+    𝐮 = zero(𝐱)
+    𝛚 = zero(𝐱)
+    p = DEMParticles(type, m, Π, r, 𝐱, 𝛉, 𝐮, 𝛚)
+    @test p.id == collect(1:10)
 end
 
 struct ParticleCellList{D, T}
